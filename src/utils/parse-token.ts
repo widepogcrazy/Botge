cee03fe7728dfae1fe7e82daf5a78a78a77d95ac
bob <>
2025-02-15 02:08:37 -0800

    store single letters in a map

--- src/utils/parse-token.ts
+++ src/utils/parse-token.ts
@@ -6,84 +6,48 @@ import { sevenTVUrlToSevenTVNotInSet } from './platform-url-to-api-url.js';
 import type { AssetInfo } from '../types.js';
 import { Platform } from '../enums.js';
 
+var letter_to_id: Map<string, string> = new Map([
+  ['A', '1f1e6'],
+  ['B', '1f1e7'],
+  ['C', '1f1e8'],
+  ['D', '1f1e9'],
+  ['E', '1f1ea'],
+  ['F', '1f1eb'],
+  ['G', '1f1ec'],
+  ['H', '1f1ed'],
+  ['I', '1f1ee'],
+  ['J', '1f1ef'],
+  ['K', '1f1f0'],
+  ['L', '1f1f1'],
+  ['M', '1f1f2'],
+  ['N', '1f1f3'],
+  ['O', '1f1f4'],
+  ['P', '1f1f5'],
+  ['Q', '1f1f6'],
+  ['R', '1f1f7'],
+  ['S', '1f1f8'],
+  ['T', '1f1f9'],
+  ['U', '1f1fa'],
+  ['V', '1f1fb'],
+  ['W', '1f1fc'],
+  ['X', '1f1fd'],
+  ['Y', '1f1fe'],
+  ['Z', '1f1ff'],
+  ['0', '30-20e3'],
+  ['1', '31-20e3'],
+  ['2', '32-20e3'],
+  ['3', '33-20e3'],
+  ['4', '34-20e3'],
+  ['5', '35-20e3'],
+  ['6', '36-20e3'],
+  ['7', '37-20e3'],
+  ['8', '38-20e3'],
+  ['9', '39-20e3']
+]);
+
 function parseSingleLetterOrNumber(token: string): string | undefined {
   const base = 'https://cdn.jsdelivr.net/gh/jdecked/twemoji/assets/72x72';
-  switch (token) {
-    case 'A':
-      return `${base}/1f1e6.png`;
-    case 'B':
-      return `${base}/1f1e7.png`;
-    case 'C':
-      return `${base}/1f1e8.png`;
-    case 'D':
-      return `${base}/1f1e9.png`;
-    case 'E':
-      return `${base}/1f1ea.png`;
-    case 'F':
-      return `${base}/1f1eb.png`;
-    case 'G':
-      return `${base}/1f1ec.png`;
-    case 'H':
-      return `${base}/1f1ed.png`;
-    case 'I':
-      return `${base}/1f1ee.png`;
-    case 'J':
-      return `${base}/1f1ef.png`;
-    case 'K':
-      return `${base}/1f1f0.png`;
-    case 'L':
-      return `${base}/1f1f1.png`;
-    case 'M':
-      return `${base}/1f1f2.png`;
-    case 'N':
-      return `${base}/1f1f3.png`;
-    case 'O':
-      return `${base}/1f1f4.png`;
-    case 'P':
-      return `${base}/1f1f5.png`;
-    case 'Q':
-      return `${base}/1f1f6.png`;
-    case 'R':
-      return `${base}/1f1f7.png`;
-    case 'S':
-      return `${base}/1f1f8.png`;
-    case 'T':
-      return `${base}/1f1f9.png`;
-    case 'U':
-      return `${base}/1f1fa.png`;
-    case 'V':
-      return `${base}/1f1fb.png`;
-    case 'W':
-      return `${base}/1f1fc.png`;
-    case 'X':
-      return `${base}/1f1fd.png`;
-    case 'Y':
-      return `${base}/1f1fe.png`;
-    case 'Z':
-      return `${base}/1f1ff.png`;
-    case '0':
-      return `${base}/30-20e3.png`;
-    case '1':
-      return `${base}/31-20e3.png`;
-    case '2':
-      return `${base}/32-20e3.png`;
-    case '3':
-      return `${base}/33-20e3.png`;
-    case '4':
-      return `${base}/34-20e3.png`;
-    case '5':
-      return `${base}/35-20e3.png`;
-    case '6':
-      return `${base}/36-20e3.png`;
-    case '7':
-      return `${base}/37-20e3.png`;
-    case '8':
-      return `${base}/38-20e3.png`;
-    case '9':
-      return `${base}/39-20e3.png`;
-    default:
-      return undefined;
-  }
+  return twemoji.base + '72x72' + '/' + letter_to_id.get('token') + '.png';
 }
 
 function parseEmoji(emoji: string): string | undefined {
