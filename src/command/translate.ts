885e50f84d3f4acd1f3287bab060649488910900
bob <>
2025-02-16 10:01:51 -0800

    format

--- src/command/translate.ts
+++ src/command/translate.ts
@@ -1,10 +1,10 @@
-import type {CommandInteraction } from 'discord.js';
+import type { CommandInteraction } from 'discord.js';
 import type { ReadonlyTranslator } from '../types.js';
 
 export function translateHandler(translator: ReadonlyTranslator) {
   return async (interaction: CommandInteraction): Promise<void> => {
     if (interaction.replied || interaction.deferred) {
-      console.warn("Skipping reply: Interaction was already handled.");
+      console.warn('Skipping reply: Interaction was already handled.');
       return;
     }
     await interaction.deferReply();
@@ -12,11 +12,14 @@ export function translateHandler(translator: ReadonlyTranslator) {
       const text = String(interaction.options.get('text')?.value);
 
       // Let DeepL auto-detect the source language by passing null
-      const result = await translator.translateText(text, null, 'en-US' , {modelType: 'latency_optimized', formality:'prefer_less'});
+      const result = await translator.translateText(text, null, 'en-US', {
+        modelType: 'latency_optimized',
+        formality: 'prefer_less'
+      });
 
       await interaction.editReply(result.text);
       return;
-    } catch (error : any) {
+    } catch (error: any) {
       const errorMessage = error instanceof Error ? error.message : String(error);
       console.error(`Error at translate --> ${errorMessage}`);
       await interaction.editReply('Failed to translate.');
