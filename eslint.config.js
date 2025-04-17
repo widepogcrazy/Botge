b926601f7d0d3f37e5f80047791e0adb05debbad
Tresster <xtressterx@gmail.com>
2025-04-17 11:22:35 +0200

    version changes, minor changes.

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,17 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default [
-  { files: ['**/*.{mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
+export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname
+        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 'latest'
       }
     }
   },
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   {
     rules: {
       '@typescript-eslint/class-methods-use-this': 'error',
@@ -32,12 +28,10 @@ export default [
       '@typescript-eslint/method-signature-style': 'error',
       //"@typescript-eslint/naming-convention": "error",
       '@typescript-eslint/no-dupe-class-members': 'error',
-      '@typescript-eslint/no-empty-interface': 'error',
       '@typescript-eslint/no-import-type-side-effects': 'error',
 
       '@typescript-eslint/no-invalid-this': 'error',
       '@typescript-eslint/no-loop-func': 'error',
-      '@typescript-eslint/no-loss-of-precision': 'error',
       //"@typescript-eslint/no-magic-numbers" : "error",
       '@typescript-eslint/no-redeclare': 'error',
 
@@ -51,7 +45,6 @@ export default [
       //"@typescript-eslint/no-unsafe-type-assertion" : "error",
       '@typescript-eslint/no-use-before-define': 'error',
       '@typescript-eslint/no-useless-empty-export': 'error',
-      '@typescript-eslint/no-var-requires': 'error',
 
       '@typescript-eslint/parameter-properties': 'error',
       '@typescript-eslint/prefer-destructuring': 'error',
@@ -72,10 +65,8 @@ export default [
         }
       ],
 
-      '@typescript-eslint/prefer-ts-expect-error': 'error',
       '@typescript-eslint/promise-function-async': 'error',
       '@typescript-eslint/require-array-sort-compare': 'error',
-      '@typescript-eslint/sort-type-constituents': 'error',
       '@typescript-eslint/strict-boolean-expressions': 'error',
       '@typescript-eslint/switch-exhaustiveness-check': 'error',
       '@typescript-eslint/typedef': 'error',
@@ -107,4 +98,4 @@ export default [
       'no-invalid-this': 'error'
     }
   }
-];
+);
