6f5d0ecafe70f8af8cdeb0050584e0d6b3d48ce0
Tresster <xtressterx@gmail.com>
2025-04-18 01:46:57 +0200

    fixed cutedog clips regex

--- src/utils/list-cutedog-clip-ids.ts
+++ src/utils/list-cutedog-clip-ids.ts
@@ -2,10 +2,16 @@ import type { ReadonlyRegExpExecArray } from '../types.js';
 
 const url =
   'https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vSWIkcAv-1T59HWffSdJwn1P4m5Kp4hjIahweIbinW2uO1TPbv2AoYH4QJL5hXL339ll3xua8ir9E8g/pub?output=html';
-const clipPattern = /https:\/\/clips\.twitch\.tv\/([A-Za-z0-9_-]+)/g;
+const clipPattern1 = /https:\/\/clips\.twitch\.tv\/([A-Za-z0-9_-]+)/g;
+const clipPattern2 = /https:\/\/www\.twitch\.tv\/cutedog_\/clip\/([A-Za-z0-9_-]+)/g;
 
 export async function listCutedogClipIds(): Promise<readonly string[]> {
   const html = await (await fetch(url)).text();
-  const matches = html.matchAll(clipPattern);
-  return Array.from(matches, (m: ReadonlyRegExpExecArray) => m[1]);
+
+  const matches1 = html.matchAll(clipPattern1);
+  const matchesArray1: readonly string[] = Array.from(matches1, (match: ReadonlyRegExpExecArray) => match[1]);
+  const matches2 = html.matchAll(clipPattern2);
+  const matchesArray2: readonly string[] = Array.from(matches2, (match: ReadonlyRegExpExecArray) => match[1]);
+
+  return [...matchesArray1, ...matchesArray2];
 }
