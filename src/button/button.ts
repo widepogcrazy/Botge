ef56e7b0b2999a83aa4f5ad9f195cd6a5fc5d678
bob <>
2025-04-28 02:33:44 -0700

    only allow owner press button and delete after send

--- src/button/button.ts
+++ src/button/button.ts
@@ -12,6 +12,10 @@ import {
 export function buttonHandler(twitchClipMessageBuilders: readonly TwitchClipMessageBuilder[]) {
   return async (interaction: ButtonInteraction): Promise<void> => {
     try {
+      if (interaction.message.interactionMetadata!.user.id !== interaction.user.id) {
+        // only allow original sender to click buttons.
+        return;
+      }
       const twitchClipMessageBuilder = twitchClipMessageBuilders.find(
         (twitchClipMessageBuilder_) =>
           twitchClipMessageBuilder_.counter === getCounterFromButtonCustomId(interaction.customId)
@@ -22,33 +26,38 @@ export function buttonHandler(twitchClipMessageBuilders: readonly TwitchClipMess
       const twitchClipInteraction = twitchClipMessageBuilder.interaction;
       const { row } = twitchClipMessageBuilder;
 
+      const defer = interaction.deferUpdate();
       if (customId.includes(PREVIOUS_BUTTON_CUSTOM_ID)) {
-        await interaction.deferUpdate();
         await twitchClipInteraction.editReply({
           embeds: [twitchClipMessageBuilder.previous()],
           components: [row]
         });
       } else if (customId.includes(NEXT_BUTTON_CUSTOM_ID)) {
-        await interaction.deferUpdate();
+        await defer;
         await twitchClipInteraction.editReply({
           embeds: [twitchClipMessageBuilder.next()],
           components: [row]
         });
       } else if (customId.includes(FIRST_BUTTON_CUSTOM_ID)) {
-        await interaction.deferUpdate();
+        await defer;
         await twitchClipInteraction.editReply({
           embeds: [twitchClipMessageBuilder.first()],
           components: [row]
         });
       } else if (customId.includes(LAST_BUTTON_CUSTOM_ID)) {
-        await interaction.deferUpdate();
+        await defer;
         await twitchClipInteraction.editReply({
           embeds: [twitchClipMessageBuilder.last()],
           components: [row]
         });
       } else if (interaction.customId.includes(SEND_CLIP_LINK_BUTTON_CUSTOM_ID)) {
-        await interaction.deferReply();
-        await interaction.editReply(twitchClipMessageBuilder.currentUrl());
+        await defer;
+        await interaction.followUp({
+          content: twitchClipMessageBuilder.currentUrl()
+        });
+        await interaction.message.delete();
+      } else {
+        console.log(`unknown button click: ${customId}`);
       }
     } catch (error) {
       console.log(`Error at button --> ${error instanceof Error ? error.message : String(error)}`);
